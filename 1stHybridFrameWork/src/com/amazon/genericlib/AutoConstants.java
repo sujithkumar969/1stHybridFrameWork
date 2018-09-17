4767808df12bfde7f4190ff5e87475323a1bf60f
SUJITH KUMAR S <SUJITH KUMAR S@DESKTOP-H1S9C19>
2018-09-17 11:27:04 +0530

    17-9-2018

--- 1stHybridFrameWork/src/com/amazon/genericlib/AutoConstants.java
+++ 1stHybridFrameWork/src/com/amazon/genericlib/AutoConstants.java
@@ -3,21 +3,21 @@ package com.amazon.genericlib;
 public interface AutoConstants {          // public static final variables are declared at interface //
 	
 	String ch_key = "webdriver.chrome.driver";
-	String ch_value = "E:\\automation tool\\Selenium Softwares(new)\\drivers\\chromedriver 2.35\\chromedriver.exe";
+	String ch_value = "E:\\My_Projects\\Work_Space_03\\git\\1stHybridFrameWork\\1stHybridFrameWork\\drivers executables\\chromedriver 2.35\\chromedriver.exe";
 	
 	String ff_key = "webdriver.gecko.driver";
-	String ff_value = "E:\\automation tool\\Selenium Softwares(new)\\drivers\\geckdriver-v0.19.1\\geckodriver.exe";
+	String ff_value = "E:\\My_Projects\\Work_Space_03\\git\\1stHybridFrameWork\\1stHybridFrameWork\\drivers executables\\geckdriver-v0.19.1\\geckodriver.exe";
 	
 	// path of Excel File //
-	String excelPath = "E:\\My_Projects\\Work_Space_03\\1stHybridFrameWork\\dataEngine\\TestData.xlsx";
+	String excelPath = "E:\\My_Projects\\Work_Space_03\\git\\1stHybridFrameWork\\1stHybridFrameWork\\dataEngine\\TestData.xlsx";
 	
     // Path of ScreenShot //
-	String screenShotDirectory = "E:\\My_Projects\\Work_Space_03\\1stHybridFrameWork\\screenShots\\";
+	String screenShotDirectory = "E:\\My_Projects\\Work_Space_03\\git\\1stHybridFrameWork\\1stHybridFrameWork\\screenShots\\";
 	
 	// Path of property file //
-	String propfilePath = "E:\\My_Projects\\Work_Space_03\\1stHybridFrameWork\\configuration\\emailConfig.properties";
+	String propfilePath = "E:\\My_Projects\\Work_Space_03\\git\\1stHybridFrameWork\\1stHybridFrameWork\\configuration\\emailConfig.properties";
 	
 	// path of Extent Report //
-	String reportPath = "E:\\My_Projects\\Work_Space_03\\1stHybridFrameWork\\reports\\report.html";
+	String reportPath = "E:\\My_Projects\\Work_Space_03\\git\\1stHybridFrameWork\\1stHybridFrameWork\\reports\\report.html";
 
 }
